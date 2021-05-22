ea1ab6afff8c6f8ff38267a2c6ffb6f2ee1a1122
Kamala Khalil <kamalakhalil04@gmail.com>
2021-05-22 15:51:03 -0400

    started us 3 dona

--- src/test/java/US003.java
+++ src/test/java/US003.java
@@ -3,6 +3,7 @@ package test.java;
 public class US003 {
     public static void main(String[] args) {
         System.out.println("Work 1 is dona");
+        System.out.println("All done!");
 
     }
 }
