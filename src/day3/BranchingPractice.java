c2a9f75b8177f185fe3e94ea926c54e0f2c6cc88
Kamala Khalil <kamalakhalil04@gmail.com>
2021-05-25 19:29:29 -0400

    first commit in review1 branch

--- src/day3/BranchingPractice.java
+++ src/day3/BranchingPractice.java
@@ -4,8 +4,12 @@ public class BranchingPractice {
     public static void main(String[] args) {
         // create new project
         //enable version control
-        //added gitignore with .idea, target folder and.iml file
+        //added .gitignore with .idea, target folder and.iml file
         // made initial commit by keeping track of all files
         // shared project on github
+
+
+        //reviewing on new branch
+
     }
 }
